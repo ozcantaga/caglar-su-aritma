48fad62cb6265ec0a4f328c82278d3a40b165143
ozcantaga <41130371+ozcantaga@users.noreply.github.com>
2026-03-21 00:12:17 +0300

    Update contact email in app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
   name: "Çağlar Su Arıtma",
         phone: "+90 5536032090 ",
         whatsapp: "905536032090",
-        email: "ozcantaga@gmail.com",
+        email: "islamerken061@gmail.com",
         googleMapsLink: "https://www.google.com/maps/place/su+aritma+PAZARCIK+%C3%87A%C4%9ELAR+SU/@37.506397,37.327095,658m/data=!3m2!1e3!4b1!4m6!3m5!1s0x1532097a717c6d89:0x158b22937ff62a4d!8m2!3d37.506397!4d37.327095!16s%2Fg%2F11z3lt2mvp?entry=ttu&g_ep=EgoyMDI2MDMxNy4wIKXMDSoASAFQAw%3D%3D", // İşletmenizin paylaşım linki
         address: "Bağdınısağır, 1061 SK dogaka çarşısı, 46700 Pazarcık/Kahramanmaraş",
        
